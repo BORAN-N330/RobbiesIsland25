5124b7d1cabe06317be67acf7cd40a0fc71c5aa0
npnorman <npn.not@gmail.com>
2025-04-12 16:35:09 -0400

    Added ammo system from bretts scripts

--- Assets/Scripts/AmmoManager.cs
+++ Assets/Scripts/AmmoManager.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AmmoManager : MonoBehaviour
 {
-    public int currentAmmo = 6;
-    public Text ammoText; // Assign the UI Text element in the Inspector
+    public int maxAmmo = 6;
+    int currentAmmo;
+    public TMP_Text ammoText; // Assign the UI Text element in the Inspector
 
     void Start()
     {
         UpdateAmmoText();
+        currentAmmo = maxAmmo;
     }
 
     public void UpdateAmmoText()
     {
+        //display the ammo text
         ammoText.text = "Ammo: " + currentAmmo.ToString();
     }
 
@@ -22,6 +26,19 @@ public class AmmoManager : MonoBehaviour
         {
             currentAmmo--;
             UpdateAmmoText();
+        }
+    }
+
+    public void ReloadAmmo() {
+        currentAmmo = maxAmmo;
+    }
+
+    public bool hasAmmo() {
+        if (currentAmmo > 0) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
