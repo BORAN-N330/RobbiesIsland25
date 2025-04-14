fea0c3fc9f96db7b32104938f554fc989c8461c5
Catsforteam <66139297+Catsforteam@users.noreply.github.com>
2025-04-14 00:13:02 -0400

    added things

--- Assets/Scripts/TeleportArea.cs
+++ Assets/Scripts/TeleportArea.cs
@@ -4,9 +4,11 @@ using UnityEngine.SceneManagement;
 public class TeleportArea : MonoBehaviour
 {
     public string sceneToTP = "";
-    private void OnCollisionEnter(Collision collision) {
-        if(collision.gameObject.tag == "Player") {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
             SceneManager.LoadScene(sceneToTP);
         }
     }
+
 }
