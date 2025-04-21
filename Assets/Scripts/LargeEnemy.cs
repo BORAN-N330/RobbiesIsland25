66b81464d997d166c43fb72779eea8beed1263de
npnorman <npn.not@gmail.com>
2025-04-20 20:02:39 -0400

    added hpBar

--- Assets/Scripts/LargeEnemy.cs
+++ Assets/Scripts/LargeEnemy.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class LargeEnemy : MonoBehaviour
 {
@@ -7,11 +9,40 @@ public class LargeEnemy : MonoBehaviour
     public Animator doorObject;
     public string animatorTrigger = "OpenDoor";
 
+    [Header("UI")]
+    public string enemyName = "Rayb-oss";
+    public Slider hpBar;
+    public Canvas canvas;
+    TMP_Text nameTag;
+    Slider hpBarIn;
+
+    RabidEnemy enemyScript;
+
+    private void Start() {
+
+        enemyScript = GetComponent<RabidEnemy>();
+
+        Debug.Log("Adding stuff");
+        hpBarIn = Instantiate(hpBar, canvas.transform);
+        nameTag = hpBarIn.transform.GetChild(0).GetComponent<TMP_Text>();
+
+        nameTag.text = enemyName;
+    }
+
+    private void Update() {
+        hpBar.maxValue = enemyScript.maxHealth;
+
+        Debug.Log(enemyScript.health);
+        hpBarIn.value = (int)enemyScript.health;
+    }
+
     private void OnDestroy() {
 
         if (doorObject != null) {
             doorObject.SetTrigger(animatorTrigger);
         }
         Debug.Log("Death");
+
+        Destroy(hpBar);
     }
 }
